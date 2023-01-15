45702c8e67d09a2aeb9ba406e0a9c25432aeab79
Thomas Petr <tpetr@rotas.coyle.club>
2023-01-15 15:21:20 -0500

    scd30

---
+++ src/manifests/scd30-prom.ts
@@ -0,0 +1,59 @@
+import { Deployment } from 'kubernetes-models/apps/v1';
+import { ConfigMap, Service } from 'kubernetes-models/v1';
+
+const name = 'scd30-prom';
+const namespace = 'monitoring';
+const metadata = { name, namespace };
+const labels = { app: name };
+
+const METRICS_PORT = 8080;
+
+export const generate = () => [
+  new Service({
+    metadata,
+    spec: {
+      ports: [
+        {
+          name: 'metrics',
+          port: METRICS_PORT
+        }
+      ],
+      selector: labels
+    }
+  }),
+  new Deployment({
+    metadata,
+    spec: {
+      replicas: 1,
+      selector: {
+        matchLabels: labels
+      },
+      template: {
+        metadata: {
+          labels
+        },
+        spec: {
+          nodeSelector: {
+            'kubernetes.io/hostname': 'mycomaster'
+          },
+          containers: [
+            {
+              name: 'scd30-prom',
+              image: 'docker.coyle.club/internal/scd30_prom:1',
+              args: [
+                '--port',
+                String(METRICS_PORT)
+              ],
+              ports: [
+                {
+                  name: 'metrics',
+                  containerPort: METRICS_PORT
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  })
+];
